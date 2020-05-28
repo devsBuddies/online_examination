24d6186d3d34b31130a4d1046b6ca375ac55789a
Prashant Singh <prashantsingh2408@gmail.com>
2020-05-28 13:48:00 +0530

    change title to Online Examination in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Online Examination
                 </div>
 
                 <div class="links">
