04f8012b12b0154e67ef9d657413af4480981fdd
M241994 <41114519+M241994@users.noreply.github.com>
2020-05-29 17:08:16 +0530

    update whole project

---
+++ app/Http/Controllers/Admin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Admin extends Controller
+{
+    public fuction index()
+	{
+		echo "Admin Controller Index Function"
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin','Admin@index');
